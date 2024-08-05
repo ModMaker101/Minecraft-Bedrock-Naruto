4de3fe6377fe559e32f5b648b1c80400e4e4a997
ModMaker101 <119018978+ModMaker101@users.noreply.github.com>
2024-08-04 23:43:47 -0400

    video and lore :(

--- packs/BP/nBehavior/scripts/examples/lore.js
+++ packs/BP/nBehavior/scripts/examples/lore.js
@@ -1,22 +1,20 @@
 import { system, world, ItemComponentTypes, ItemStack } from "@minecraft/server";
 
-const diamond_sword = "naruto:kunai";
+const your_custom_item = "naruto:kunai";
 
 function checkPlayerInventory() {
   for (const player of world.getPlayers()) {
     const inventory = player.getComponent('minecraft:inventory').container;
     for (let i = 0; i < inventory.size; i++) {
       const item = inventory.getItem(i);
-      if (item && item.typeId === diamond_sword) {
-        item.setLore(['§r§l§0you must work', 'you have to work']);
-        inventory.setItem(i, item); // Update the item in the inventory
-      }
-    }
+      if (item && item.typeId === your_custom_item) {
+        item.setLore(['§r§l§0hi this is 1 line', 'wow a second line', 'you can only have 20 lines and 50 characters']);
+
+        inventory.setItem(i, item);
+      }      
+    }    
   }
-
-  // Schedule the next check
-  system.runTimeout(checkPlayerInventory, 10); // 20 ticks = 1 second
+  system.runTimeout(checkPlayerInventory, 10);
 }
-
 // Start the periodic check
 checkPlayerInventory();
