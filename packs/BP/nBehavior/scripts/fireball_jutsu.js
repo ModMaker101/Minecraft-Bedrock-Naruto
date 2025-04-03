0fee4a65dae26cdcdd6cae9fa523388f1c083af6
ModMaker101 <119018978+ModMaker101@users.noreply.github.com>
2025-04-03 09:33:57 -0400

    fireball and scripting

---
+++ packs/BP/nBehavior/scripts/fireball_jutsu.js
@@ -0,0 +1,14 @@
+import { world } from "@minecraft/server";
+
+export function fireballHandler() {
+    world.afterEvents.itemUse.subscribe((event) => {
+        const { itemStack, source: player } = event;
+        console.log(itemStack.typeId);
+        // Check if the used item is naruto:fireball_jutsu
+        if (itemStack.typeId === 'naruto:fireball_jutsu') {
+            // Execute the commands
+            player.runCommand('playsound jutsu @a[x=~,y=~,z=~,r=10]');
+            player.runCommand('particle minecraft:basic_smoke_particle ^^1.6^0.8');
+        }
+    });
+}
--- packs/BP/nBehavior/scripts/main.js
+++ packs/BP/nBehavior/scripts/main.js
@@ -1,8 +1,12 @@
 import { checkPlayerInventory } from "./items/lore";
 import { system, world } from "@minecraft/server";
 checkPlayerInventory();
+
 import { setupEmojiListCommand, setupHelpCommand, setupHelpListCommand } from "./emojis";
 // Setup the list and help commands
 setupEmojiListCommand();
 setupHelpListCommand();
 setupHelpCommand();
+
+import { fireballHandler } from "./fireball_jutsu";
+fireballHandler();
